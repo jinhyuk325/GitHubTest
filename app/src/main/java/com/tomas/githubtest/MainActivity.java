b8ea4d3ba6dfbe0262707894606c5e10d77c1227
jin hyuk <jinhyuk325@nate.com>
2018-04-26 14:34:48 +0900

    FistCommit

--- app/src/main/java/com/tomas/githubtest/MainActivity.java
+++ app/src/main/java/com/tomas/githubtest/MainActivity.java
@@ -2,12 +2,15 @@ package com.tomas.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.tv);
+        tv.setText("SEX!");
     }
 }
